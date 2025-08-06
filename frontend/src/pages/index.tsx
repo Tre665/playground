cb050152f41318abf2c7b56a9df1b9a1b774c3c0
tre665 <19689069+Tre665@users.noreply.github.com>
2025-08-06 15:16:07 +0200

    feat(frontend): :construction: add a simple second page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Page() {
   return (
-    <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-      Hello, Next.js!
-    </h1>
+    <>
+      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Weather App
+      </h1>
+      <Link href="/dashboard">Dashboard</Link>
+    </>
   );
 }
