42f2e694669159ac4fe2e0b4803705f77fb5924c
tre665 <19689069+Tre665@users.noreply.github.com>
2025-08-07 11:30:24 +0200

    refactor(backend): :bricks: migrate away from fastify-cli

---
+++ backend/src/main.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify';
+import dotenv from 'dotenv';
+import { app } from './app';
+
+dotenv.config();
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 5000;
+
+// Instantiate Fastify with some config
+const server = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+server.register(app);
+
+// Start listening.
+server.listen({ port, host }, (err) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  } else {
+    console.log(`[ ready ] http://${host}:${port}`);
+  }
+});
