a1f64c1cb50026cafc8dba602caaa7d5e4203403
tre665 <19689069+Tre665@users.noreply.github.com>
2025-08-08 13:13:37 +0200

    feat(backend): :sparkles: add initial POST route for a widget

--- backend/src/types/widgets.types.ts
+++ backend/src/types/widgets.types.ts
@@ -1,9 +1,7 @@
-import { ObjectId } from '@fastify/mongodb';
-
 /** Represents a weather widget for a specific city */
 export interface Widget {
   /** The _id of the widget */
-  _id: ObjectId;
+  _id: string;
   /** The city name */
   location: string;
   /** The date and time the widget was created */
@@ -11,18 +9,13 @@ export interface Widget {
   /** The date and time when any field of the widget was last updated */
   updatedAt?: Date;
   /** The user ID of the user who created the widget */
-  userId?: ObjectId | null;
+  userId?: string | null;
 }
 
-/**
- * Represents the data needed to create a new widget in the data store
- */
-export interface CreateWidget {
+/** The payload for creating a new widget */
+export interface CreateWidgetPayload {
+  /** The city name */
   location: string;
-  /** The date and time the widget was created */
-  createdAt: Date;
-  /** The date and time when any field of the widget was last updated */
-  updatedAt: Date;
   /** The user ID of the user who created the widget */
-  userId: ObjectId | null;
+  userId?: string;
 }
