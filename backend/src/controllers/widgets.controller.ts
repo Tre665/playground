f016c3285df434bc36768d5f009a3b61271e5c9a
tre665 <19689069+Tre665@users.noreply.github.com>
2025-08-08 13:13:37 +0200

    feat(backend): :sparkles: add initial POST route for a widget

--- backend/src/controllers/widgets.controller.ts
+++ backend/src/controllers/widgets.controller.ts
@@ -1,8 +1,39 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { Widget } from '../models/widgets.models';
+import { CreateWidget, Widget } from '../models/widgets.models';
+import { CreateWidgetPayload } from '../types/widgets.types';
+import { ObjectId } from '@fastify/mongodb';
 
+/** Gets all widgets from the data store */
 export const getAllWidgets = async (request: FastifyRequest, reply: FastifyReply) => {
   const db = request.server.mongo?.db;
   const widgets = await db?.collection<Widget>('widgets').find().toArray();
   return reply.send(widgets);
+};
+
+/** Adds a new widget to the data store */
+export const addWidget = async (
+  request: FastifyRequest<{ Body: CreateWidgetPayload }>,
+  reply: FastifyReply,
+) => {
+  const { location, userId } = request.body;
+
+  if (!location) {
+    return reply.status(400).send({ message: 'Location is required' });
+  }
+
+  const newWidget: CreateWidget = {
+    location,
+    userId: userId ? new ObjectId(userId) : null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  const db = request.server.mongo?.db;
+  const result = await db?.collection<CreateWidget>('widgets').insertOne(newWidget);
+
+  if (!result?.insertedId || !result?.insertedId) {
+    return reply.status(500).send({ message: 'Failed to create widget' });
+  }
+
+  return reply.status(201).send({ _id: result.insertedId, ...newWidget });
 };
--- backend/src/routes/widgets.routes.ts
+++ backend/src/routes/widgets.routes.ts
@@ -1,5 +1,6 @@
-import { FastifyInstance, FastifyPluginAsync } from 'fastify';
-import { getAllWidgets } from '../controllers/widgets.controller';
+import { FastifyInstance, FastifyPluginAsync, FastifyRequest } from 'fastify';
+import { addWidget, getAllWidgets } from '../controllers/widgets.controller';
+import { CreateWidgetPayload } from '../types/widgets.types';
 
 const widgets: FastifyPluginAsync = async (
   server: FastifyInstance,
@@ -8,6 +9,13 @@ const widgets: FastifyPluginAsync = async (
   server.get('/widgets', async function (request, reply) {
     return getAllWidgets(request, reply);
   });
+
+  server.post(
+    '/widgets',
+    async function (request: FastifyRequest<{ Body: CreateWidgetPayload }>, reply) {
+      return addWidget(request, reply);
+    },
+  );
 };
 
 export default widgets;
