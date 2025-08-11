ca9430bf9378e2cce96213b54cb256b390ad8fbd
tre665 <19689069+Tre665@users.noreply.github.com>
2025-08-11 16:02:33 +0200

    refactor(backend): :coffin: remove not needed interfaces

--- backend/src/models/widget.mappings.ts
+++ backend/src/models/widget.mappings.ts
@@ -1,12 +1,14 @@
 import { WeatherApiResponse } from '../types/open-weather.types';
 import { WeatherWidget, Widget } from './widgets.models';
 
+/** combines the data of a persisted widget with weather data*/
 export const mapWeatherDataToWidget = (
   widget: Widget,
   weatherData: WeatherApiResponse,
 ): WeatherWidget => {
   return {
     ...widget,
+    // only temperature for now, more data would be available
     weatherData: {
       temperature: weatherData.main.temp,
     },
